f47a52502c6f4d095318994f809e9f008d896c17
Hibiki <valentin.engelhard@eemi.com>
2021-06-09 11:51:57 +0200

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,32 @@
 <template>
-  <section>
-      <nav class="navbar navbar-expand-lg navbar-light" style="width: 100%;">
-        <div class="container-fluid" style="display: flex; justify-content: center; width: 100%;">
-          <a class="navbar-brand" href="#" style="display: flex; justify-content: center;">
-            <img src="./assets/logo.png" alt="Logo Game'Her" style="width: 20%; height: auto;">
-          </a>
-        </div>
-      </nav>
-      <Profile/>
-      <h2 class="mt-5 mb-5">------</h2>
-      <AdminTicket/>
-      <h2 class="mt-5 mb-5">------</h2>
-    <PoleGestionTicket/>
-    <h2 class="mt-5 mb-5">------</h2>
-    
-  </section>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import AdminTicket from './components/AdminTicket.vue'
-import PoleGestionTicket from './components/PoleGestionTicket.vue'
-import Profile from './components/Profile.vue'
-
-export default {
-  name: 'App',
-  components: {
-    PoleGestionTicket,
-    AdminTicket,
-    Profile
-  }
-}
-</script>
-
 <style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-  color: white;
-  background-image: url("assets/background.jpg");
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
